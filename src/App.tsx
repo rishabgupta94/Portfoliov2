66da0207400bde06dbbc2e3b897174b7cd9a22c0
Rishab Gupta <rishabgupta98@gmail.com>
2022-07-11 16:32:45 -0300

    moves theme to another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { NavBar } from 'components/NavBar';
 import { About } from 'pages/About';
 import { Skills } from 'pages/Skills';
-
-// const themeLight = createTheme({
-// 	palette: {
-// 		background: {
-// 			default: '#e4f0e2',
-// 		},
-// 	},
-// });
-
-// TODO: Move this to another file
-const themeDark = createTheme({
-	palette: {
-		text: {
-			primary: '#C0CBEB',
-			secondary: '#985757',
-		},
-		background: { default: '#1D1D1D', navbar: '#121212' },
-	},
-});
+import { themeDark } from 'utils/theme';
 
 function App() {
 	/**
---
+++ src/utils/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material';
+
+export const themeLight = createTheme({
+	palette: {
+		background: {
+			default: '#e4f0e2',
+		},
+	},
+});
+
+export const themeDark = createTheme({
+	palette: {
+		text: {
+			primary: '#C0CBEB',
+			secondary: '#985757',
+		},
+		background: { default: '#1D1D1D', navbar: '#121212' },
+	},
+});
