af119afff6e24d231db8d4b3f1f7c8f519386e4a
Rishab Gupta <rishabgupta98@gmail.com>
2022-07-20 23:16:39 -0300

    makes the drawer responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import {
 	Drawer,
+	IconButton,
 	List,
 	ListItem,
 	ListItemButton,
@@ -7,8 +8,13 @@ import {
 	useTheme,
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ROUTES } from '../constants/Routes';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const DRAWER_WIDTH_DESKTOP = 100;
+const DRAWER_WIDTH_MOBILE = 250;
 
 const drawerItems = [
 	{
@@ -52,6 +58,14 @@ export const NavBar = () => {
 	const theme = useTheme();
 	const classes = useStyles(theme);
 	const location = useLocation().pathname;
+	const container =
+		window !== undefined ? () => window.document.body : undefined;
+
+	const [mobileOpen, setMobileOpen] = useState(false);
+
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
 
 	const activeRoute = (path: ROUTES) => {
 		if (location.includes(path)) return true;
@@ -59,42 +73,98 @@ export const NavBar = () => {
 	};
 
 	return (
-		<Drawer
-			sx={{
-				width: 100,
-				flexShrink: 0,
-				'& .MuiDrawer-paper': {
+		<>
+			<IconButton
+				color="inherit"
+				aria-label="open drawer"
+				edge="start"
+				onClick={handleDrawerToggle}
+				sx={{ mr: 2, ml: 1, display: { sm: 'none' } }}
+			>
+				<MenuIcon fontSize="large" />
+			</IconButton>
+			<Drawer
+				container={container}
+				variant="temporary"
+				open={mobileOpen}
+				onClose={handleDrawerToggle}
+				ModalProps={{
+					keepMounted: true, // Better open performance on mobile.
+				}}
+				PaperProps={{
+					sx: {
+						backgroundColor: theme.palette.background.navbar,
+						display: 'flex',
+						justifyContent: 'center',
+					},
+				}}
+				sx={{
+					display: { xs: 'block', sm: 'none' },
 					width: 100,
-					boxSizing: 'border-box',
-				},
-			}}
-			PaperProps={{
-				sx: {
-					backgroundColor: theme.palette.background.navbar,
-					display: 'flex',
-					justifyContent: 'center',
-				},
-			}}
-			anchor="left"
-			variant="permanent"
-		>
-			<List>
-				{drawerItems.map((item, i) => (
-					<Link key={i} to={item.path} className={classes.link}>
-						<ListItem key={i} disablePadding>
-							<ListItemButton
-								selected={activeRoute(item.path)}
-								className={classes.selected}
-							>
-								<ListItemText
-									classes={{ primary: classes.listItemText }}
-									primary={item.name}
-								/>
-							</ListItemButton>
-						</ListItem>
-					</Link>
-				))}
-			</List>
-		</Drawer>
+					flexShrink: 0,
+					'& .MuiDrawer-paper': {
+						width: DRAWER_WIDTH_MOBILE,
+						boxSizing: 'border-box',
+					},
+				}}
+				anchor="left"
+			>
+				<List>
+					{drawerItems.map((item, i) => (
+						<Link key={i} to={item.path} className={classes.link}>
+							<ListItem key={i} disablePadding>
+								<ListItemButton
+									selected={activeRoute(item.path)}
+									className={classes.selected}
+								>
+									<ListItemText
+										classes={{ primary: classes.listItemText }}
+										primary={item.name}
+									/>
+								</ListItemButton>
+							</ListItem>
+						</Link>
+					))}
+				</List>
+			</Drawer>
+			<Drawer
+				sx={{
+					display: { xs: 'none', sm: 'block' },
+					width: 100,
+					flexShrink: 0,
+					'& .MuiDrawer-paper': {
+						width: DRAWER_WIDTH_DESKTOP,
+						boxSizing: 'border-box',
+					},
+				}}
+				PaperProps={{
+					sx: {
+						backgroundColor: theme.palette.background.navbar,
+						display: 'flex',
+						justifyContent: 'center',
+					},
+				}}
+				anchor="left"
+				variant="permanent"
+			>
+				<List>
+					{drawerItems.map((item, i) => (
+						<Link key={i} to={item.path} className={classes.link}>
+							<ListItem key={i} disablePadding>
+								<ListItemButton
+									selected={activeRoute(item.path)}
+									className={classes.selected}
+								>
+									<ListItemText
+										classes={{ primary: classes.listItemText }}
+										primary={item.name}
+									/>
+								</ListItemButton>
+							</ListItem>
+						</Link>
+					))}
+				</List>
+			</Drawer>
+		</>
 	);
 };
