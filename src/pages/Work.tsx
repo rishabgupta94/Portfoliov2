757c79d7a0120409317f58681cf07b23d6cd7216
Rishab Gupta <rishabgupta98@gmail.com>
2022-07-30 16:09:14 -0300

    adds selected item to list

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -6,12 +6,22 @@ import {
 	ListItemText,
 	useTheme,
 } from '@mui/material';
+import { useState } from 'react';
 
 // eslint-disable-next-line quotes
 const workItems = ['ReelData AI', 'Perennia', "St. Mary's Uni", 'Optymyze'];
 
 export const Work = () => {
 	const theme = useTheme();
+	const [selectedItem, setSelectedItem] = useState(0);
+
+	const onListItemClick = (
+		_: React.MouseEvent<HTMLDivElement, MouseEvent>,
+		index: number
+	) => {
+		setSelectedItem(index);
+	};
+
 	return (
 		<Grid
 			container
@@ -24,8 +34,8 @@ export const Work = () => {
 				{workItems.map((item, i) => (
 					<ListItem key={i} disablePadding>
 						<ListItemButton
-						// selected={activeRoute(item.path)}
-						// className={classes.selected}
+							selected={selectedItem === i}
+							onClick={(event) => onListItemClick(event, i)}
 						>
 							<ListItemText
 								// classes={{ primary: classes.listItemText }}
